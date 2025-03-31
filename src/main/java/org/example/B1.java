7820af72a2a07b0a1f7c1745df3b304e7f0b7d8e
Default-HV <vuthuyhienjob@gmail.com>
2025-03-31 18:07:39 +0700

    first commit

---
+++ src/main/java/org/example/B1.java
@@ -0,0 +1,22 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class B1 {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        //Khai bao
+        int songuyen=1;
+        char kytu='H';
+        String Chuoi="My full name is Vu Thuy Hien";
+        boolean GiaTriLogic= false;
+
+        //In ra man hinh
+        System.out.println("So nguyen la: " + songuyen);
+        System.out.println("Ky tu: " + kytu);
+        System.out.println("Noi dung: " + Chuoi);
+        System.out.println("Gia tri logic: " + GiaTriLogic);
+    }
+    }
